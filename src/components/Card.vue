dbd3c8b6909ec18c1ba3bd85218468dd34c14385
ellinor.carlstedt@teliacompany.com <ellinor.carlstedt@teliacompany.com>
2018-10-10 13:10:28 +0200

    cards can be flipped with transition effects

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,18 @@
 <template>
-    <div>
+    <transition-group name="flip">
+    <div class="card"
+         key="image"
+         v-show="flipped"
+         v-on:click="flipCard">
         <img :src="fullImageSource"
              class="card-img">
     </div>
+        <div class="card"
+             key="noImage"
+             v-show="!flipped"
+             v-on:click="flipCard">
+        </div>
+    </transition-group>
 </template>
 
 <script>
@@ -16,6 +26,11 @@
               flipped: false,
           }
         },
+        methods: {
+            flipCard() {
+                this.flipped = !this.flipped;
+            }
+        },
         computed: {
               fullImageSource: function () {
                   return require(`@/images/${this.img}`);
@@ -24,11 +39,40 @@
     }
 </script>
 
-<style scoped>
+<style>
+    * {
+        box-sizing: border-box;
+    }
 
     .card-img {
+        height: 100%;
+        width: 100%;
+    }
+
+    .card {
         height: 12rem;
         width: 18rem;
+        margin-right: 1rem;
+        margin-bottom: 1rem;
+        border-radius: 1rem;
+        box-shadow: 0.3rem 0.3rem 0.3rem lightgrey;
+        overflow: hidden;
     }
+
+    .flip-enter-active {
+        transition: all 0.4s ease;
+    }
+
+    .flip-leave-active {
+        display: none;
+    }
+
+    .flip-enter, .flip-leave {
+        transform: rotateY(180deg);
+        opacity: 0;
+
+    }
+
+
 
 </style>
